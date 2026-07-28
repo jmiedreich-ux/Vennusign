c5f2130a9b963fe15e173d61022686151ae86243
jmiedreich-ux <jmiedreich@gmail.com>
2026-07-28 01:05:28 -0400

    WP-03.03 extend subscription repository contract

--- src/Vennu.Data/Repositories/IVenueSubscriptionRepository.cs
+++ src/Vennu.Data/Repositories/IVenueSubscriptionRepository.cs
@@ -4,6 +4,7 @@ namespace Vennu.Data.Repositories;
 
 public interface IVenueSubscriptionRepository
 {
+    Task<IReadOnlyCollection<VenueSubscription>> GetAllAsync(CancellationToken cancellationToken = default);
     Task<VenueSubscription?> GetByVenueIdAsync(Guid venueId, CancellationToken cancellationToken = default);
     Task<bool> SaveAsync(VenueSubscription subscription, CancellationToken cancellationToken = default);
 }
